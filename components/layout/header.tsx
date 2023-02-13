a00a6a3ce456bbfa40e0d564b72e2763bfa4f73f
Tom Benbow <tom@hellomoon.net>
2023-02-13 11:32:11 -0600

    Keep logo up top on mobile

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -176,10 +176,10 @@ export const Header = ({
         </div>
 
         {/* Mobile Nav */}
+        <Logo className="absolute top-8 left-4 w-10" />
         <div className={`${sectionClasses} hidden md:block h-screen fixed z-40 top-0 left-0 right-0`}>
           <div style={backgroundStyles} className={`${backgroundClasses} ${background} transition duration-400 absolute w-full h-screen -z-1`}></div>
           <div className={`w-full md:p-5`}>
-            <Logo className="absolute top-3 left-4 w-10" />
             <div className={`absolute top-3 -right-2 py-1 pl-2 pr-6 rounded ${nav?.navBackgroundColor}`} onClick={() => setNavOpen(!navOpen)}>
               <Burger color="white" isOpen={navOpen}  />
             </div>
